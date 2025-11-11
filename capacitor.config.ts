4df9621c4d3be7fb9d8328c5662a812636fce372
Mael Gruand <maelgruand7@gmail.com>
2025-11-11 14:40:31 +0100

    Adding android config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.maeldeveloppement.lightnav',
+  appName: 'Light Navigation',
+  webDir: 'dist'
+};
+
+export default config;
